789c01903ad30e793e89566f0334ce3c914c4230
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2023-10-13 06:58:14 +0200

    picocss-setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,21 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   standalone: true,
   template: `
+    <main class="container">
       <h1>{{title}}</h1>
+      <div class="grid">
+        <div><button class="app-button" type="button">A Button</button></div>
+      </div>
+
+      <article class="black-background">An article</article>
+    </main>
+
   `,
   styles: [
     `
+      .black-background {
+        background-color: var(--h1-color);
+      }
     `
   ],
 })
