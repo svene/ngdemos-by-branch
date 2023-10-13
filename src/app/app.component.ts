374b477c93215b89d3015db02fe14b351a62df63
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2023-10-13 06:54:06 +0200

    make app standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  standalone: true,
+  template: `
+      <h1>{{title}}</h1>
+  `,
+  styles: [
+    `
+    `
+  ],
 })
 export class AppComponent {
   title = 'ngdemos-by-branch';
