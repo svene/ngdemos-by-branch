79fbf5d5512cefd30a654883230cfd6e270e6372
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2023-10-13 14:44:31 +0200

    custom-input component: implemented as ControlValueAccessor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {CustomInputComponent} from "../custom-input/custom-input.component";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,9 @@ import {CustomInputComponent} from "../custom-input/custom-input.component";
     <main class="container">
       <h1>{{title}}</h1>
       <div class="grid">
-        <app-custom-input></app-custom-input>
+        <app-custom-input [(ngModel)]="external" name="externalVal"></app-custom-input>
       </div>
+      <div>external value: {{external}}</div>
 
       <article class="black-background">An article</article>
     </main>
@@ -24,9 +26,11 @@ import {CustomInputComponent} from "../custom-input/custom-input.component";
   ],
   imports: [
     AppComponent,
-    CustomInputComponent
+    CustomInputComponent,
+    FormsModule
   ]
 })
 export class AppComponent {
   title = 'ngdemos-by-branch';
+  external = '';
 }
--- src/custom-input/custom-input.component.ts
+++ src/custom-input/custom-input.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, forwardRef} from '@angular/core';
+import {ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR} from "@angular/forms";
 
 @Component({
   selector: 'app-custom-input',
   standalone: true,
-  template: `<input/>`,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => CustomInputComponent),
+      multi: true
+    }
+  ],
+  template: `<input [(ngModel)]="value"/>internal value: {{_internalValue}}`,
   styles: [
     `
       .black-background {
@@ -11,7 +19,44 @@ import { Component } from '@angular/core';
       }
     `
   ],
+  imports: [
+    FormsModule
+  ]
 })
-export class CustomInputComponent {
-  title = 'ngdemos-by-branch';
+export class CustomInputComponent implements ControlValueAccessor {
+  onChange: any = () => {
+  }
+  onTouch: any = () => {
+  }
+
+  _internalValue = ""
+
+  set value(val: string) {  // this value is updated by programmatic changes
+    if (val !== undefined && this._internalValue !== val) {
+      this._internalValue = val
+      this.onChange(val)
+      this.onTouch(val)
+    }
+  }
+
+  get value() {
+    return this._internalValue;
+  }
+
+  // this method sets the value programmatically
+  writeValue(value: any) {
+    this.value= value
+  }
+
+  // upon UI element value changes, this method gets triggered
+  registerOnChange(fn: any) {
+    this.onChange = fn
+  }
+
+  // upon touching the element, this method gets triggered
+  registerOnTouched(fn: any) {
+    this.onTouch = fn
+  }
+
 }
+
